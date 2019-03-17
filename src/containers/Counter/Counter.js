051b1b04d44398bc4338abe55c4b6f384336dedb
Ryan Moore <rhcmoore@gmail.com>
2019-03-16 20:07:54 -0700

    Save and disply results on click

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -9,33 +9,40 @@ class Counter extends Component {
         counter: 0
     }
 
-    counterChangedHandler = ( action, value ) => {
-        switch ( action ) {
+    counterChangedHandler = (action, value) => {
+        switch (action) {
             case 'inc':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + 1 } } )
+                this.setState((prevState) => { return { counter: prevState.counter + 1 } })
                 break;
             case 'dec':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - 1 } } )
+                this.setState((prevState) => { return { counter: prevState.counter - 1 } })
                 break;
             case 'add':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + value } } )
+                this.setState((prevState) => { return { counter: prevState.counter + value } })
                 break;
             case 'sub':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
+                this.setState((prevState) => { return { counter: prevState.counter - value } })
                 break;
             default:
-                this.setState( ( prevState ) => { return { counter: prevState.counter + 0} } )
+                this.setState((prevState) => { return { counter: prevState.counter + 0 } })
         }
     }
 
-    render () {
+    render() {
         return (
             <div>
-                <CounterOutput value={this.props.ctr} /> 
+                <CounterOutput value={this.props.ctr} />
                 <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
-                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
-                <CounterControl label="Add 5" clicked={this.props.onAddCounter}  />
-                <CounterControl label="Subtract 5" clicked={this.props.onSubtractCounter}  />
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter} />
+                <CounterControl label="Add 5" clicked={this.props.onAddCounter} />
+                <CounterControl label="Subtract 5" clicked={this.props.onSubtractCounter} />
+                <hr />
+                <button onClick={this.props.onSaveResult}>Save Result</button>
+                <ul>
+                    {this.props.storedResults.map(result => (
+                        <li key={result.id} onClick={this.props.onDeleteResult}>{result.value}</li>
+                    ))}
+                </ul>
             </div>
         );
     }
@@ -44,8 +51,8 @@ class Counter extends Component {
 // instructions on how state should be mapped to props used in container
 const mapStateToProps = state => {
     return {
-        // in the form of prop ctr, give value of counter in global state 
-        ctr: state.counter
+        ctr: state.counter, // in the form of prop ctr, give value of counter in global state 
+        storedResults: state.results
     };
 };
 
@@ -54,10 +61,12 @@ const mapDispatchToProps = dispatch => {
     // object that defines prop names that hold reference to function, 
     // which when executed dispatch an action
     return {
-        onIncrementCounter: () => dispatch({type:"INCREMENT"}),
-        onDecrementCounter: () => dispatch({type:"DECREMENT"}),
-        onAddCounter: () => dispatch({type: "ADD", val: 5}),
-        onSubtractCounter: () => dispatch({type: "SUBTRACT", val: 5})
+        onIncrementCounter: () => dispatch({ type: "INCREMENT" }),
+        onDecrementCounter: () => dispatch({ type: "DECREMENT" }),
+        onAddCounter: () => dispatch({ type: "ADD", val: 5 }),
+        onSubtractCounter: () => dispatch({ type: "SUBTRACT", val: 5 }),
+        onSaveResult: () => dispatch({ type: "SAVE_RESULT" }),
+        onDeleteResult: () => dispatch({ type: "DELETE_RESULT" })
     }
 }
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,29 +1,38 @@
 const initialState = {
-    counter: 0
+    counter: 0,
+    results: []
 }
 
+// sets a new state
 const reducer = (state = initialState, action) => {
-    if (action.type === "INCREMENT") {
-        return {
-            counter: state.counter + 1
-        }
+    switch (action.type) {
+        case "INCREMENT":
+            const newState = Object.assign({}, state); // copy, but not a deep clone, of state
+            newState.counter = state.counter + 1; // change values of new state
+            return newState; // return new state object
+        case "DECREMENT":
+            return {
+                ...state, // distribute properties/values in new object - IMMUTABLE
+                counter: state.counter - 1 // add this property to object or update if present
+            }
+        case "ADD":
+            return {
+                ...state,
+                counter: state.counter + action.val
+            }
+        case "SUBTRACT":
+            return {
+                ...state,
+                counter: state.counter - action.val
+            }
+        case "SAVE_RESULT":
+            return {
+                ...state,
+                results: state.results.concat({id: new Date(), value: state.counter})  // concat returns new array - IMMUTABLE (not like .push)
+            }
+        default:
+            return state;
     }
-    if (action.type === "DECREMENT") {
-        return {
-            counter: state.counter - 1
-        }
-    }
-    if (action.type === "ADD") {
-        return {
-            counter: state.counter + action.val
-        }
-    }
-    if (action.type === "SUBTRACT") {
-        return {
-            counter: state.counter - action.val
-        }
-    }
-    return state;
 }
 
 export default reducer;
