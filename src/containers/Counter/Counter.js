2cb08dd1a77673001271746b3d82c89069714a43
Ryan Moore <rhcmoore@gmail.com>
2019-03-16 18:25:37 -0700

    implement incrementer with Redux

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from "react-redux";
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
@@ -22,14 +23,16 @@ class Counter extends Component {
             case 'sub':
                 this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
                 break;
+            default:
+                this.setState( ( prevState ) => { return { counter: prevState.counter + 0} } )
         }
     }
 
     render () {
         return (
             <div>
-                <CounterOutput value={this.state.counter} />
-                <CounterControl label="Increment" clicked={() => this.counterChangedHandler( 'inc' )} />
+                <CounterOutput value={this.props.ctr} /> 
+                <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
                 <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
                 <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
                 <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler( 'sub', 5 )}  />
@@ -38,4 +41,22 @@ class Counter extends Component {
     }
 }
 
-export default Counter;
+// instructions on how state should be mapped to props used in container
+const mapStateToProps = state => {
+    return {
+        // in the form of prop ctr, give value of counter in global state 
+        ctr: state.counter
+    };
+};
+
+// which kinds of actions we dispatch in this container
+const mapDispatchToProps = dispatch => {
+    // object that defines prop names that hold reference to function, 
+    // which when executed dispatch an action
+    return {
+        onIncrementCounter: () => dispatch({type:"INCREMENT"})
+    }
+}
+
+// connect is a function that returns a hoc
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
